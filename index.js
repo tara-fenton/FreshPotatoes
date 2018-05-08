54c1c6a772e993ee10bc209ba1d558d48b330b15
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-05-08 14:46:05 -0400

    connect to database

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-const sqlite = require('sqlite'),
-      Sequelize = require('sequelize'),
-      request = require('request'),
-      express = require('express'),
-      app = express();
+const sqlite = require("sqlite"),
+  Sequelize = require("sequelize"),
+  request = require("request"),
+  express = require("express"),
+  app = express();
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+const {
+  PORT = 3000,
+  NODE_ENV = "development",
+  DB_PATH = "./db/database.db"
+} = process.env;
+const API =
+  "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1";
 
 // START SERVER
 Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
-  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+  .then(() =>
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+  )
+  .catch(err => {
+    if (NODE_ENV === "development") console.error(err.stack);
+  });
+
+const sequelize = new Sequelize("database", "username", "password", {
+  host: "host",
+  dialect: "sqlite",
+  storage: DB_PATH
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection established successfully");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 // ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
+app.get("/films/:id/recommendations", getFilmRecommendations);
 
 // ROUTE HANDLER
-function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
-}
+function getFilmRecommendations(req, res) {}
 
 module.exports = app;
