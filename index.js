623cb828127b64ca468ecce4df1cd24d56dacac0
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-05-08 15:39:13 -0400

    single quote

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const sqlite = require("sqlite"),
-  Sequelize = require("sequelize"),
-  request = require("request"),
-  express = require("express"),
+const sqlite = require('sqlite'),
+  Sequelize = require('sequelize'),
+  request = require('request'),
+  express = require('express'),
   app = express();
 
 const {
   PORT = 3000,
-  NODE_ENV = "development",
-  DB_PATH = "./db/database.db"
+  NODE_ENV = 'development',
+  DB_PATH = './db/database.db'
 } = process.env;
 const API =
-  "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1";
+  'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1';
 
 // START SERVER
 Promise.resolve()
@@ -18,32 +18,33 @@ Promise.resolve()
     app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
   )
   .catch(err => {
-    if (NODE_ENV === "development") console.error(err.stack);
+    if (NODE_ENV === 'development') console.error(err.stack);
   });
 
-const sequelize = new Sequelize("database", "username", "password", {
-  host: "host",
-  dialect: "sqlite",
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'host',
+  dialect: 'sqlite',
   storage: DB_PATH
 });
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection established successfully");
+    console.log('Connection established successfully');
   })
   .catch(err => {
-    console.error("Unable to connect to the database:", err);
+    console.error('Unable to connect to the database:', err);
   });
 
 // ROUTES
-app.get("/films/:id/recommendations", getFilmRecommendations);
+app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
 
 }
-app.get("*", (req, res) => {
+// handles missing routes
+app.get('*', (req, res) => {
   res.status(404).json({
     message: '"message" key missing'
   })
