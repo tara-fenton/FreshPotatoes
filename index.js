86eaefb9460eaf008181ac83a128a24c870ee174
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-05-09 22:23:21 -0400

    genre for recommended films

--- index.js
+++ index.js
@@ -77,6 +77,8 @@ app.get("/films/:id/recommendations", getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
+  let limit = 0,
+    offset = 0;
   // find the film from the id in the params in the route
   Film.findById(req.params.id, {})
     .then(film => {
@@ -84,6 +86,10 @@ function getFilmRecommendations(req, res) {
       const releaseDate = moment(film.release_date);
       const startDate = releaseDate.subtract("years", 15).format("YYYY-MM-DD");
       const endDate = releaseDate.add("years", 15).format("YYYY-MM-DD");
+
+      // find the genre name for the recommended films
+      Genre.findById(film.genre_id).then(genre => {
+
 
       // Find all the films with The same genre as the parent film and
       // Been released within 15 years, before or after the parent film
@@ -108,49 +114,75 @@ function getFilmRecommendations(req, res) {
           // call the reviews API with the ids
           request(`${API}?films=${film_ids_api}`, (err, response, body) => {
             const reviewsJSON = JSON.parse(body);
-            console.log(JSON.parse(body));
 
             // A minimum of 5 reviews
             const reviewsOverFive = reviewsJSON.filter(review => {
               return review.reviews.length >= 5;
             });
-
+            // console.log(reviewsOverFive);
             // An average rating greater than 4.0
             const reviewsAverage = reviewsOverFive.map(review => {
-                // get the total of the review ratings
-                const total = review.reviews.reduce((sum, val) => {
-                  return sum + val.rating;
-                }, 0);
-                // get the average from the the total ratings
-                const averageRating = total / review.reviews.length;
-                // set it to the review
-                review.average_rating = averageRating;
+              // get the total of the review ratings
+              const total = review.reviews.reduce((sum, val) => {
+                return sum + val.rating;
+              }, 0);
+              // get the average from the the total ratings
+              const averageRating = total / review.reviews.length;
+              // set it to the review
+              review.average_rating = averageRating;
 
-                return review;
-              });
-                // An average rating greater than 4.0
-              const reviewesOverFourAverage = reviewsAverage.filter(review => {
-                return review.average_rating > 4;
-              });
-
-              // map the ids to use in the recommendations
-              const reviewIds = reviewesOverFourAverage.map(film => {
-                return film.film_id;
-              });
-
-
-            res.json({
-              recommendations: reviewesOverFourAverage  
+              return review;
+            });
+            // console.log(reviewsAverage);
+            // An average rating greater than 4.0
+            const reviewesOverFourAverage = reviewsAverage.filter(review => {
+              return review.average_rating > 4;
             });
 
+            // map the ids to use in the recommendations
+            const reviewIds = reviewesOverFourAverage.map(film => {
+              return film.film_id;
+            });
 
+            Film.all({
+              attributes: ["id", "title", "release_date"],
+              where: { id: { in: reviewIds } },
+              order: ["id"]
+            }).then(recommendedFilms => {
+              const finalRecommendedFilms = recommendedFilms.map(film => {
+                const matchedFilm = reviewesOverFourAverage.filter(element => {
+                  // console.log('element', element);
+                  // console.log('film', film);
+                  return (element.film_id = film.id);
+                });
 
+                // console.log('matchedFilm', matchedFilm[0]);
+
+                return {
+                  id: matchedFilm[0].film_id,
+                  title: film.title,
+                  releaseDate: film.release_date,
+                  genre: genre.name,
+                  averageRating: matchedFilm[0].average_rating,
+                  reviews: matchedFilm[0].reviews.length
+                };
+              });
+
+              res.json({
+                recommendations: finalRecommendedFilms,
+                meta: {
+                  limit: limit,
+                  offset: offset
+                }
+              });
+            });
           });
         })
 
         .catch(error => {
           console.log(error);
         });
+      })
     })
     .catch(error => {
       console.log("film error: ", error);
